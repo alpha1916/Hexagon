8732de346db3806db58d1c4620b35153672f0256
alpha <alpha1916@163.com>
2020-08-07 16:39:20 +0800

    td2

--- web-mobile/src/settings.76ae7.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/MainScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"45e63",resources:"1cf6e",main:"94e3d"}};
